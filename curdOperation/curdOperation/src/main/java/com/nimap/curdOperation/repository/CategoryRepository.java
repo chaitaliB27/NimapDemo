2b21b38f91aa8b0ad526ce0c604bad10d0deb136
chaitaliB27 <145447766+chaitaliB27@users.noreply.github.com>
2025-01-28 13:12:50 +0530

    Delete curdOperation/curdOperation directory

--- curdOperation/curdOperation/src/main/java/com/nimap/curdOperation/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nimap.curdOperation.repository;
-
-import com.nimap.curdOperation.Entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
